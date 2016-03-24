3c79228d13fd76b5cea156c6f5dc487556e291fe
Andrew Bryant <ar.bryant@btinternet.com>
2016-03-24 00:30:32 +0000

    Optimize webpack in production

--- config/webpack/prod.config.js
+++ config/webpack/prod.config.js
@@ -1,1 +1,11 @@
-module.exports = {}
+const webpack = require('webpack')
+
+module.exports = {
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new webpack.optimize.UglifyJsPlugin,
+    new webpack.optimize.DedupePlugin
+  ]
+}
